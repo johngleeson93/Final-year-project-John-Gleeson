a385b20b9491c588ccee1452a58d051af84239d9
John Gleeson <john.gleeson93@gmail.com>
2022-02-03 21:27:36 +0000

    Signup and login added
    Signup and login added

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const Exercises = require("./app/controllers/exercises");
 
 module.exports = [
   { method: "GET", path: "/", config: Exercises.index },
+  { method: "GET", path: "/signup", config: Exercises.signup },
+  { method: "GET", path: "/login", config: Exercises.login },
   {
     method: "GET",
     path: "/{param*}",
